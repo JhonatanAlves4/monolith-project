c7b5195fa72ca336538c7408b42605d263f5fb6e
Jhonatan Alves <jhonatan4alves@gmail.com>
2023-12-26 09:38:01 -0300

    feat: add client usecase and gateway

---
+++ src/modules/client-adm/usecase/add-client/add-client.usecase.ts
@@ -0,0 +1,37 @@
+import Id from "../../../@shared/domain/value-object/id.value-object";
+import Client from "../../domain/client.entity";
+import ClientGateway from "../../gateway/client.gateway";
+import {
+  AddClientInputDto,
+  AddClientOutputDto,
+} from "./add-client.usecase.dto";
+
+export default class AddClientUsecase {
+  private _clientRepository: ClientGateway;
+
+  constructor(clientRepositort: ClientGateway) {
+    this._clientRepository = clientRepositort;
+  }
+
+  async execute(input: AddClientInputDto): Promise<AddClientOutputDto> {
+    const props = {
+      name: input.name,
+      email: input.email,
+      address: input.address,
+    };
+
+    const client = new Client(props);
+    await this._clientRepository.add(client);
+
+    const output: AddClientOutputDto = {
+      id: client.id.id,
+      name: client.name,
+      email: client.email,
+      address: client.address,
+      createdAt: client.createdAt,
+      updatedAt: client.updatedAt,
+    };
+
+    return output;
+  }
+}
